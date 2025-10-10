ab5663a106eb0e47c2f7c749ea5feed5f0a8eba0
gavin jin <gavinjin62@gmail.com>
2025-10-09 20:53:08 -0700

    add tanstack reactrouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppRouter } from "./router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +16,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AppRouter />
     </QueryClientProvider>
   </React.StrictMode>
 );
